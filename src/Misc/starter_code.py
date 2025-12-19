2b701cea896f7d279c585bc6a4f8abd6fb632989
Ava <avahom16@gmail.com>
2025-12-19 16:56:19 -0500

    Reorganized personal Advent of Code solution

--- src/Misc/starter_code.py
+++ src/Misc/starter_code.py
@@ -1,4 +1,4 @@
 def parse_data() -> str:
-    with open("/src/data.txt", "r") as data:
+    with open("C:/Users/soona/IdeaProjects/AdventOfCode/src/", "r") as data:
         parsed = data.read()
         return parsed
