c5f24cd4034b8cc9eb8fa931e7f8b7013a465242
Ava <avahom16@gmail.com>
2025-12-20 17:13:10 -0500

    2021 Day 2

---
+++ src/2021/2021Day2.py
@@ -0,0 +1,41 @@
+from src.misc.starter_code import parse_file
+
+
+def part1_answer() -> int:
+    coord = [0, 0]
+    for line in data:
+        line = line.split(' ')
+        magnitude = int(line[1])
+        match line[0]:
+            case "forward":
+                coord[0] += magnitude
+            case "down":
+                coord[1] += magnitude
+            case "up":
+                coord[1] -= magnitude
+    return coord[0] * coord[1]
+
+
+def part2_answer() -> int:
+    coord = [0, 0]
+    aim = 0
+    for line in data:
+        line = line.split(' ')
+        magnitude = int(line[1])
+        match line[0]:
+            case "down":
+                aim += magnitude
+            case "up":
+                aim -= magnitude
+            case "forward":
+                coord[0] += magnitude
+                coord[1] += (magnitude * aim)
+    return coord[0] * coord[1]
+
+
+
+data = parse_file()
+data = data.split('\n')
+
+print(f"""Part 1 answer: {part1_answer()}
+Part 2 answer: {part2_answer()}""")
---
+++ src/2022/2022Day2tbd.py
@@ -0,0 +1,27 @@
+from src.misc.starter_code import parse_file
+
+def rps_result(opp: str, choice: str) -> int:
+    curr_result = ""
+    match choice:
+        case "X": curr_result = "Draw"
+        case "Y": curr_result = "Win"
+        case "Z": curr_result = "Loss"
+    return 0
+
+
+def round_result(opp: str, choice: str) -> int:
+    curr_result = 0
+    match choice:
+        case "X": curr_result += 1
+        case "Y": curr_result += 2
+        case "Z": curr_result += 3
+    return curr_result
+
+data = parse_file()
+data = data.split('\n')
+
+result = 0
+for line in data:
+    line = line.split(' ')
+    result += round_result(line[0], line[1])
+print(result)
