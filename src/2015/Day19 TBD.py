bd19534bce9f150508649482f4c11ae46dec47cf
Ava <avahom16@gmail.com>
2025-12-25 22:17:35 -0500

    2015 Day 19 Part 1

--- src/2015/Day19 TBD.py
+++ src/2015/Day19 TBD.py
@@ -13,4 +13,10 @@ for line in data:
     line = line.split(' ')
     conversions[line[0]].append(line[-1])
 
-print(conversions, molecule)
+seen_molecules = set()
+for k in conversions:
+    for v in conversions[k]:
+        for idx, l in enumerate(molecule):
+            if k == molecule[idx:idx + len(k)]:
+                seen_molecules.add(molecule[:idx] + molecule[idx:].replace(k, v, 1))
+print(f"Part one answer: {len(seen_molecules)}")
