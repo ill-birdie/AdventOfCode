5e0cfe0b8cf50d9e3a23cf539f0c62646bd0af50
Ava <avahom16@gmail.com>
2025-12-30 16:03:50 -0500

    Began Day 6 Part 2

--- src/2015/Day06 TBD.py
+++ src/2015/Day06 TBD.py
@@ -4,12 +4,28 @@ start_time = time.perf_counter()
 
 
 class Solution:
-    def __init__(self):
+    def __init__(self, mode: str):
+        """
+        Initializes the Solution class
+
+        :param mode: Determines how the execute function behaves:
+        'boolean' toggles each coordinate (on/off)
+        'integer' increments/decrements each coordinate's brightness (int)
+        Defaults to boolean if argument is neither
+        """
+        self._total_brightness = 0
+
+        modes = {'boolean', 'integer'}
+        self._mode = mode.lower()
+        if self._mode not in modes:
+            self._mode = 'boolean'
+
         self._grid = {}
         for x in range(1000):
             for y in range(1000):
                 l = [x, y]
                 self._grid[str(l)] = False
+
 
     def __getitem__(self, coord):
         return self._grid[coord]
@@ -17,6 +33,14 @@ class Solution:
     @property
     def grid(self):
         return self._grid
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @property
+    def total_brightness(self):
+        return self._total_brightness
 
 
     def execute(self, cmd: str, s: list[int], e: list[int]) -> None:
@@ -27,17 +51,35 @@ class Solution:
         for x in range(start_x, end_x + 1):
             for y in range(start_y, end_y + 1):
                 coord = f'[{x}, {y}]'
-                result: bool
-                match cmd:
-                    case 'on': result = True
-                    case 'off': result = False
-                    case 'toggle': result = not self._grid[coord]
-                    case _: continue
-                self._grid[coord] = result
+                if self._mode == 'boolean':
+                    match cmd:
+                        case 'on':
+                            result = True
+                        case 'off':
+                            result = False
+                        case 'toggle':
+                            result = not self._grid[coord]
+                        case _: continue
+                    self._grid[coord] = result
+                elif self._mode == 'integer':
+                    match cmd:
+                        case 'on':
+                            result = 1
+                        case 'off':
+                            result = -1
+                        case 'toggle':
+                            result = 2
+                        case _:
+                            continue
+                    self._grid[coord] += result
+                    self._total_brightness += result
+                    if self._total_brightness < 0:
+                        self._total_brightness = 0
 
 data = parse_file()
 data = data.split('\n')
-foo = Solution()
+part1 = Solution('boolean')
+part2 = Solution('integer')
 
 for line in data:
     line = line.split(' ')
@@ -46,18 +88,23 @@ for line in data:
     end = [int(n) for n in coordinates[1].split(',')]
 
     if line[0] == 'toggle':
-        foo.execute('toggle', start, end)
+        part1.execute('toggle', start, end)
+        part2.execute('toggle', start, end)
     else:
         command = line[1]
-        foo.execute(command, start, end)
+        part1.execute(command, start, end)
+        part2.execute(command, start, end)
 
 part1_answer = 0
-for light in foo.grid:
-    if foo.grid[light]:
+for light in part1.grid:
+    if part1.grid[light]:
         part1_answer += 1
 
+"""part two is giving me the wrong answer for some reason. i don't know why.
+i'll bugtest the next time i check in on this problem"""
+
 print(f"""Part one answer: {part1_answer}
-Part two answer: """)
+Part two answer: {part2.total_brightness}""")
 
 end_time = time.perf_counter()
-print(f'Program took {round(end_time - start_time, 3)} seconds to finish executing')
+print(f'\nProcess took {round(end_time - start_time, 3)} seconds to finish executing')
