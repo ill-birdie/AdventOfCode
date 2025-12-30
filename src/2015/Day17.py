b88c0f16437a550a782834fc2f616f9911440bb7
Ava <avahom16@gmail.com>
2025-12-30 00:14:22 -0500

    Day 17 solution wrapped inside a class (data encapsulation)

--- src/2015/Day17.py
+++ src/2015/Day17.py
@@ -1,33 +1,39 @@
 from src.misc.starter_code import parse_file
 
+class Solution:
+    combinations = 0
+    depth = 1
+    combination_lengths = []
+
+    # Empty constructor
+    def __init__(self):
+        pass
+
+    def eval_combos(self, target: int, containers: list) -> None:
+        """
+        Determines the number of ways target (int) can be broken up using numbers in containers (list).
+        :param target: The number being broken up.
+        :param containers: The possible numbers used to break up target (int).
+        :return: None; the function updates instances variables within the class.
+        """
+        for idx, container in enumerate(containers):
+            curr_diff = target - container
+            if curr_diff == 0:
+                self.combinations += 1
+                self.combination_lengths.append(self.depth)
+            elif curr_diff > 0:
+                self.depth += 1
+                self.eval_combos(curr_diff, containers[idx + 1:])
+        self.depth -= 1
+
 data = sorted(parse_file().split('\n'), key=int, reverse=True)
 data = [int(n) for n in data]
 
-result = 0
-curr_depth = 1
-combo_lengths = []
-def eval_combos(curr_target: int, curr_containers: list) -> None:
-    """
-    A recursive function that finds and returns the total number of possible ways to
-    add up curr_containers' (list) elements to reach curr_target (int) by
-    incrementing result (int, variable in outer scope).
-    """
-    global result
-    global curr_depth
-    global combo_lengths
-    for idx, container in enumerate(curr_containers, start=1):
-        curr_diff = curr_target - container
-        if curr_diff == 0:
-            result += 1
-            combo_lengths.append(curr_depth)
-        elif curr_diff > 0:
-            curr_depth += 1
-            eval_combos(curr_diff, curr_containers[idx:])
-    curr_depth -= 1
+foo = Solution()
+num_liters = 150
+foo.eval_combos(num_liters, data)
+min_length = min(foo.combination_lengths)
+num_min = len([branch for branch in foo.combination_lengths if branch == min_length])
 
-
-eval_combos(150, data)
-min_length = min(combo_lengths)
-num_min = len([d for d in combo_lengths if d == min_length])
-print(f"""Part one answer: {result}
+print(f"""Part one answer: {foo.combinations}
 Part two answer: {num_min}""")
