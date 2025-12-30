ce37343095fdfd6358520e4b94356849761d85b6
Ava <avahom16@gmail.com>
2025-12-29 23:46:47 -0500

    Solved Day 7

--- src/2015/Day07 TBD.py
+++ src/2015/Day07 TBD.py
@@ -29,56 +29,47 @@ def operand_int(op) -> int:
 data = parse_file()
 data = data.split('\n')
 
-"""
-i need to account for keyerrors caused by variables not already being initialized (false assumption)
-im probably going to use a while-loop to check if the data list is empty, removing elements that
-successfully perform the operation. that way we can keep cycling through the instructions
-until we can finally use them after everything is initialized.
-
-keyerrors are addressed. now i need to figure out how to remove the lines we've just looked at and used 
-also a new bug emerged with the indices and popping the data. i think it skips an element if i pop the previous one.
-"""
-idx = 0
 virtual_stack = {}
-for line in data:
-    line = line.split(' -> ')
-    left_side = line[0]
-    init_var = line[1]
-    parsed_left = left_side.split(' ')
-    print(init_var, idx)
+checked_indices = set()
+while len(checked_indices) < len(data):
+    checked_indices = set()
+    for idx, line in enumerate(data):
+        line = line.split(' -> ')
+        left_side = line[0]
+        init_var = line[1]
+        parsed_left = left_side.split(' ')
 
-    curr_result = -1
-    match len(parsed_left):
-        # Initialize a new variable
-        case 1:
-            curr_result = operand_int(left_side)
+        curr_result = -1
+        match len(parsed_left):
+            # Initialize a new variable
+            case 1:
+                curr_result = operand_int(left_side)
 
-        # Bitwise NOT operator
-        case 2:
-            operand = parsed_left[1]
-            if operand in virtual_stack:
-                var = virtual_stack[operand]
-                curr_result = wrap(~var)
+            # Bitwise NOT operator
+            case 2:
+                operand = parsed_left[1]
+                if operand in virtual_stack:
+                    var = virtual_stack[operand]
+                    curr_result = wrap(~var)
 
-        # Other bitwise operators
-        case 3:
-            operator = parsed_left[1]
-            operand1 = operand_int(parsed_left[0])
-            operand2 = operand_int(parsed_left[-1])
-            if operand1 != -1 and operand2 != -1:
-                match operator:
-                    case 'AND':
-                        curr_result = operand1 & operand2
-                    case 'OR':
-                        curr_result = operand1 | operand2
-                    case 'LSHIFT':
-                        curr_result = operand1 << operand2
-                    case 'RSHIFT':
-                        curr_result = operand1 >> operand2
-    if curr_result != -1:
-        virtual_stack[init_var] = curr_result
-        data.pop(idx)
-    else:
-        idx += 1
-print(virtual_stack)
-print(data)
+            # Other bitwise operators
+            case 3:
+                operator = parsed_left[1]
+                operand1 = operand_int(parsed_left[0])
+                operand2 = operand_int(parsed_left[-1])
+                if operand1 != -1 and operand2 != -1:
+                    match operator:
+                        case 'AND':
+                            curr_result = operand1 & operand2
+                        case 'OR':
+                            curr_result = operand1 | operand2
+                        case 'LSHIFT':
+                            curr_result = operand1 << operand2
+                        case 'RSHIFT':
+                            curr_result = operand1 >> operand2
+        if curr_result != -1 and idx not in checked_indices:
+            virtual_stack[init_var] = curr_result
+            checked_indices.add(idx)
+
+print(f"""Part one answer: {virtual_stack['a']}""")
+print(virtual_stack['b'])
