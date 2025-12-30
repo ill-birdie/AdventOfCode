a065bf59759742d7dd2c86f023c1066acc05f644
Ava <avahom16@gmail.com>
2025-12-30 04:21:44 -0500

    2015 Day 8

--- src/2015/Day08.py
+++ src/2015/Day08.py
@@ -1,5 +1,15 @@
 from src.misc.starter_code import parse_file
 
+def convert(s: str) -> str:
+    converted = ['"']
+    special_chars = {"'", '"', '\\'}
+    for letter in s:
+        if letter in special_chars:
+            converted.append('\\')
+        converted.append(letter)
+    converted.append('"')
+    return ''.join(converted)
+
 def get_memory(s: str) -> int:
     num_chars = 0
     i = 1
@@ -19,8 +29,10 @@ data = parse_file()
 data = data.split('\n')
 
 part1_answer = 0
+part2_answer = 0
 for line in data:
     part1_answer += get_memory(line)
+    part2_answer += get_memory(convert(line))
 
 print(f"""Part one answer: {part1_answer}
-Part two answer: """)
+Part two answer: {part2_answer}""")
