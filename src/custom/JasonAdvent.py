5ea0e527de7d49a2a764455b42a77442f70b76eb
Ava <avahom16@gmail.com>
2026-01-06 22:00:57 -0500

    Completed a Custom Advent

---
+++ src/custom/JasonAdvent.py
@@ -0,0 +1,34 @@
+from src.misc.starter_code import parse_file
+
+month_days = {}
+for month in range(1, 13):
+    days = 31
+    if month == 2:
+        days = 28
+    elif month in {4, 6, 9, 11}:
+        days = 30
+    month_days[month] = days
+
+
+def month_valid(m: int) -> bool:
+    return m in range(1, 13)
+
+def day_valid(m: int, d: int) -> bool:
+    if month_valid(m):
+        return d in range(1, month_days.get(m) + 1)
+    else:
+        return False
+
+def year_valid(y: int) -> bool:
+    return y in range(2000, 2100 + 1)
+
+
+data = parse_file().split('/')
+data = [int(n) for n in data]
+
+
+result = 0
+for month, day, year in zip(data, data[1:], data[2:]):
+    if day_valid(month, day) and year_valid(year):
+        result += (month + day + year)
+print(result)
