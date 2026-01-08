d09231a866637374477676e577878906ed1b6473
Ava <avahom16@gmail.com>
2026-01-08 00:06:41 -0500

    Began Day 6 Part 2

--- src/2025/Day06 TBD.py
+++ src/2025/Day06 TBD.py
@@ -1,8 +1,10 @@
 from src.starter_code import parse_file
+from typing import List
 from functools import reduce
 import operator
 
-def parse_line(s: str) -> list:
+
+def line_nums(s: str) -> list:
     return [c for c in s.split(' ') if c != '']
 
 
@@ -18,13 +20,39 @@ def list_answer(l: list) -> int:
     return reduce(curr_op, nums)
 
 
+def init_grid1(grid: list) -> List[List[int]]:
+    new_grid = [[c] for c in line_nums(grid[0])]
+    for l in grid[1:]:
+        l = line_nums(l)
+        for idx, d in enumerate(l):
+            new_grid[idx].append(d)
+    return new_grid
+
+
+def init_grid2(grid: list) -> List[List[int]]:
+    new_grid = []
+    longest_len = 0
+    curr_len = len(grid[0])
+    for l in grid[1:]:
+        if curr_len > longest_len:
+            longest_len = curr_len
+        curr_len = len(l)
+
+    """
+    Iterate through each column of the list longest_len times.
+    Each column joined will be a number.
+    Have a list to store the current operation.
+    Have a string to store the current number (column).
+    If the bottom row is an operation, append the list to the grid.
+    Otherwise, append the current number to the operation list.
+    
+    This should separate each operation based on if there's a new operation in the bottom row.
+    """
+
+
 data = parse_file().split('\n')
-part1_grid = [[c] for c in parse_line(data[0])]
-part2_grid = []
-for line in data[1:]:
-    line = parse_line(line)
-    for idx, d in enumerate(line):
-        part1_grid[idx].append(d)
+part1_grid = init_grid1(data)
+part2_grid = init_grid2(data)
 
 part1_answer = 0
 for line in part1_grid:
